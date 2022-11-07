9754b655d717e4f66afb1c3c4cc3d44370544583
odaialazzeh <odai.alazzeh@gmail.com>
2022-11-07 13:24:30 +0200

    Add JS file

---
+++ index.js
@@ -0,0 +1,14 @@
+/* add new book */
+import addfunction from './module/add.js';
+
+/* add date */
+import time from './module/date.js';
+
+/* show books */
+import Data from './module/show.js';
+
+/* remove books */
+import buttonPressed from './module/remove.js';
+
+/* hide parts */
+import hide from './module/hide.js';
